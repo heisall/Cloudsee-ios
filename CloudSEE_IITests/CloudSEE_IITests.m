4b329565ef61a240087d9f52c6d63de339149ec2
Chenzhenyang <chenzhenyang@jovision.com>
2014-09-22 17:41:43 +0800

    1.初始化项目

---
+++ CloudSEE_IITests/CloudSEE_IITests.m
@@ -0,0 +1,34 @@
+//
+//  CloudSEE_IITests.m
+//  CloudSEE_IITests
+//
+//  Created by chenzhenyang on 14-9-22.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CloudSEE_IITests : XCTestCase
+
+@end
+
+@implementation CloudSEE_IITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
