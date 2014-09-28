be7c738d0f6fee605123f2f1813e83cebef87bbf
Chenzhenyang <chenzhenyang@jovision.com>
2014-09-28 18:10:51 +0800

    1.添加顶部工具条和下拉列表的基类2.添加通道列表选择界面

---
+++ CloudSEE_II/JVCBaseTopToolViewAndDropListViewController.h
@@ -0,0 +1,79 @@
+//
+//  JVCBaseTopToolViewAndDropListViewController.h
+//  CloudSEE_II
+//
+//  Created by chenzhenyang on 14-9-28.
+//  Copyright (c) 2014年 chenzhenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JVCTopToolBarView.h"
+#import "JVCAnimationHelper.h"
+#import "JVCEditViewControllerDropListViewCell.h"
+
+static const CGFloat        kDropTableViewHeight                 = 46.0f;  //下拉设备列表的Cell高度
+static const NSTimeInterval kOperationViewAnimationScaleBig      = 0.7;    //功能按钮点击变大的延时时间
+static const NSTimeInterval kOperationViewAnimationScaleRestore  = 0.5;    //功能按钮点击变大后恢复的延时时间
+static const NSTimeInterval kDropListViewAnimationBegin          = 0.8;    //下拉视图下拉的延时时间
+static const NSTimeInterval kDropListViewAnimationEnd            = 0.5;    //下拉视图收回的延时时间
+
+@interface JVCBaseTopToolViewAndDropListViewController : UIViewController <UIGestureRecognizerDelegate,UITableViewDataSource,UITableViewDelegate,JVCTopToolBarViewDelegate> {
+    
+    int                nIndex ;
+    UITableView       *deviceListTableView;
+    UIImageView       *dropImageView;
+    
+    NSArray           *titles;
+    JVCTopToolBarView *toolBarView;         //顶部工具条
+}
+
+/**
+ *  初始化布局
+ */
+-(void)initWithTopToolView;
+
+/**
+ *  单击事件
+ *
+ *  @param recognizer 单击手势对象
+ */
+-(void)dropDownCilck:(UITapGestureRecognizer*)recognizer;
+
+/**
+ *  初始化功能区域按钮
+ */
+-(void)initWithOperationView;
+
+/**
+ *  初始化下拉的设备表格视图
+ */
+-(void)initWithDropDeviceListView;
+
+/**
+ *  初始化滑动事件
+ */
+-(void)initWithSwipeGestureRecognizer;
+
+/**
+ *  左右滑动切换设备
+ *
+ *  @param swipeGestureRecognizer 滑动的对象
+ */
+-(void)swipeGestureRecognizer:(UISwipeGestureRecognizer *)swipeGestureRecognizer;
+
+
+/**
+ *  滑动结束处理逻辑
+ */
+-(void)swipeGestureRecognizerEnd:(UISwipeGestureRecognizerDirection)direction;
+
+/**
+ *  下拉结束处理逻辑
+ */
+-(void)dropDownCilckEnd;
+
+#pragma mark -------------JVCTopToolBarView delegate
+
+-(void)topItemSelectedIndex:(int)index;
+
+@end
---
+++ CloudSEE_II/JVCBaseTopToolViewAndDropListViewController.m
@@ -0,0 +1,338 @@
+//
+//  JVCBaseTopToolViewAndDropListViewController.m
+//  CloudSEE_II
+//
+//  Created by chenzhenyang on 14-9-28.
+//  Copyright (c) 2014年 chenzhenyang. All rights reserved.
+//
+
+#import "JVCBaseTopToolViewAndDropListViewController.h"
+
+@interface JVCBaseTopToolViewAndDropListViewController ()
+
+@end
+
+@implementation JVCBaseTopToolViewAndDropListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        
+        /**
+         *  解决父类UIViewController带导航条添加ScorllView坐标系下沉64像素的问题（ios7）
+         
+         */
+        
+        if ([self respondsToSelector:@selector(edgesForExtendedLayout)])
+        {
+            self.edgesForExtendedLayout = UIRectEdgeNone;
+        }
+        
+    }
+    return self;
+}
+
+
+/**
+ *  初始化布局
+ */
+-(void)initWithTopToolView{
+    
+    titles = [[NSArray alloc] initWithObjects:@"A361",@"A35555555562",@"A3633456",@"A361",@"A35555555562",@"A366666663",@"A36444444441",@"A35555555562",@"A36333333",@"A361",@"A35555555562",@"A3633456",@"A361",@"A35555555562",@"A366666663",@"A36444444441",@"A35555555562",@"A36333333", nil];
+    
+    CGRect toolViewRect = CGRectMake(0.0, 0.0f, self.view.frame.size.width, 0.0);
+    
+    toolBarView          = [[JVCTopToolBarView alloc] initWithFrame:toolViewRect];
+    toolBarView.jvcTopToolBarViewDelegate   = self;
+    [toolBarView initWithLayout:titles];
+    [self.view addSubview:toolBarView];
+    [toolBarView release];
+    
+    UIImage *topBarDropImage   = [UIImage imageNamed:@"edi_topBar_dropBtn.png"];
+    
+    dropImageView    = [[UIImageView alloc] init];
+    dropImageView.frame           = CGRectMake(self.view.frame.size.width - topBarDropImage.size.width,toolBarView.frame.origin.y , topBarDropImage.size.width, topBarDropImage.size.height);
+    dropImageView.backgroundColor = [UIColor clearColor];
+    dropImageView.image           = topBarDropImage;
+    dropImageView.userInteractionEnabled = YES;
+    [self.view addSubview:dropImageView];
+    
+    //添加单击事件
+    UITapGestureRecognizer *dropClickRecognizer;
+    
+    dropClickRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dropDownCilck:)];
+    dropClickRecognizer.numberOfTapsRequired = 1;
+    [dropImageView addGestureRecognizer:dropClickRecognizer];
+    [dropClickRecognizer release];
+    
+    [dropImageView release];
+}
+
+/**
+ *  单击事件
+ *
+ *  @param recognizer 单击手势对象
+ */
+-(void)dropDownCilck:(UITapGestureRecognizer*)recognizer
+{
+    if ([recognizer state] == UIGestureRecognizerStateEnded) {
+        
+        if (deviceListTableView.frame.size.height <= 0.0f) {
+            
+            [UIView animateWithDuration:kDropListViewAnimationBegin animations:^{
+                
+                self.view.backgroundColor = [UIColor blackColor];
+                deviceListTableView.frame = CGRectMake(deviceListTableView.frame.origin.x, recognizer.view.frame.origin.y, deviceListTableView.frame.size.width, self.view.frame.size.height);
+                [self.view bringSubviewToFront:recognizer.view];
+                recognizer.view.transform =  CGAffineTransformMakeRotation(-180 * M_PI/180.0);
+                
+                
+                
+            } completion:^(BOOL finished){
+                
+            }];
+            
+        }else {
+            
+            [UIView animateWithDuration:kDropListViewAnimationEnd animations:^{
+                
+                self.view.backgroundColor = [UIColor clearColor];
+                deviceListTableView.frame = CGRectMake(deviceListTableView.frame.origin.x, deviceListTableView.frame.origin.y, deviceListTableView.frame.size.width, 0.0);
+                recognizer.view.transform =  CGAffineTransformIdentity;
+                
+                
+            }completion:^(BOOL finished){
+                
+                 [deviceListTableView reloadData];
+                
+            }];
+        }
+        
+    }
+}
+
+/**
+ *  初始化功能区域按钮
+ */
+-(void)initWithOperationView{
+
+    
+
+}
+
+/**
+ *  初始化下拉的设备表格视图
+ */
+-(void)initWithDropDeviceListView{
+    
+    deviceListTableView            = [[UITableView alloc] init];
+    deviceListTableView.delegate   = self;
+    deviceListTableView.dataSource = self;
+    deviceListTableView.frame      = CGRectMake(0.0, 0.0, self.view.frame.size.width, 0.0);
+    [deviceListTableView setSeparatorColor:[UIColor clearColor]]; //去掉UITableViewCell的边框
+    [self.view addSubview:deviceListTableView];
+    [deviceListTableView release];
+}
+
+/**
+ *  初始化滑动事件
+ */
+-(void)initWithSwipeGestureRecognizer{
+    
+    UISwipeGestureRecognizer *swipeGestureRecognizer;
+    
+    swipeGestureRecognizer =[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGestureRecognizer:)];
+    [swipeGestureRecognizer setDirection:UISwipeGestureRecognizerDirectionRight];
+    [self.view addGestureRecognizer:swipeGestureRecognizer];
+    [swipeGestureRecognizer release];
+    
+    swipeGestureRecognizer =[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGestureRecognizer:)];
+    [swipeGestureRecognizer setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [self.view addGestureRecognizer:swipeGestureRecognizer];
+    [swipeGestureRecognizer release];
+    
+}
+
+/**
+ *  左右滑动切换设备
+ *
+ *  @param swipeGestureRecognizer 滑动的对象
+ */
+-(void)swipeGestureRecognizer:(UISwipeGestureRecognizer *)swipeGestureRecognizer{
+    
+    BOOL changeStatus = FALSE;
+    
+    switch (swipeGestureRecognizer.direction) {
+            
+        case UISwipeGestureRecognizerDirectionLeft:{
+            
+            if (nIndex > 0) {
+                
+                nIndex --;
+                [toolBarView setSelectedTopItemAtIndex:nIndex];
+                changeStatus = TRUE;
+            }
+            
+            break;
+        }
+        case UISwipeGestureRecognizerDirectionRight:{
+            
+            if (nIndex < titles.count -1) {
+                
+                nIndex ++;
+                [toolBarView setSelectedTopItemAtIndex:nIndex];
+                changeStatus = TRUE;
+            }
+            
+            break;
+        }
+        default:
+            break;
+    }
+    
+    if (changeStatus) {
+        
+        [self swipeGestureRecognizerEnd:swipeGestureRecognizer.direction];
+    }
+}
+
+/**
+ *  滑动结束处理逻辑
+ */
+-(void)swipeGestureRecognizerEnd:(UISwipeGestureRecognizerDirection)direction{
+    
+    [[JVCAnimationHelper shareJVCAnimationHelper] startWithAnimation:self.view exchangeSubviewAtIndex:0 withSubviewAtIndex:0 duration:0.7 animationType:kJVCAnimationMacroCube animationSubType: direction == UISwipeGestureRecognizerDirectionLeft?kCATransitionFromRight:kCATransitionFromLeft];
+
+}
+
+#pragma mark -------------JVCTopToolBarView delegate
+
+-(void)topItemSelectedIndex:(int)index {
+    
+    DDLogVerbose(@"%s----%d",__FUNCTION__,index);
+    
+    [[JVCAnimationHelper shareJVCAnimationHelper] startWithAnimation:self.view exchangeSubviewAtIndex:0 withSubviewAtIndex:0 duration:0.7 animationType:kJVCAnimationMacroCube animationSubType:index > nIndex ? kCATransitionFromLeft : kCATransitionFromRight];
+    
+    nIndex = index;
+}
+
+#pragma mark ---------- deviceListTableView dataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return  titles.count;
+    
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+    
+}
+
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *cellIndentify = @"cellIndentifiy";
+    
+    JVCEditViewControllerDropListViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIndentify];
+    
+    if (cell == nil) {
+        
+        cell = [[[JVCEditViewControllerDropListViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIndentify] autorelease];
+        
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    
+    for (UIView *viewContent in cell.contentView.subviews) {
+        
+        [viewContent removeFromSuperview];
+        viewContent =nil;
+    }
+    
+    cell.contentView.backgroundColor = [UIColor clearColor];
+    
+    [cell initWithLayoutView:@"A3678900000"];
+    
+    [cell setViewSelectedView:indexPath.row == nIndex];
+    
+    return cell;
+}
+
+#pragma mark ------- deviceListTableView delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (nIndex != indexPath.row) {
+        
+        nIndex = indexPath.row;
+    }
+    
+    [self dropDownCilckEnd];
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return kDropTableViewHeight;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    
+    UIImage *topBarDropImage   = [UIImage imageNamed:@"edi_topBar_dropBtn.png"];
+    
+    return topBarDropImage.size.height;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section;    {
+    
+    return [NSString stringWithFormat:@"共%d个设备", titles.count];
+    
+}
+
+/**
+ *  下拉结束处理逻辑
+ */
+-(void)dropDownCilckEnd{
+
+    [UIView animateWithDuration:kDropListViewAnimationEnd animations:^{
+        
+        self.view.backgroundColor = [UIColor clearColor];
+        deviceListTableView.frame = CGRectMake(deviceListTableView.frame.origin.x, deviceListTableView.frame.origin.y, deviceListTableView.frame.size.width, 0.0);
+        dropImageView.transform   =  CGAffineTransformIdentity;
+        [toolBarView setSelectedTopItemAtIndex:nIndex];
+        
+    }];
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self initWithTopToolView];
+    [self initWithOperationView];
+    [self initWithDropDeviceListView];
+    [self initWithSwipeGestureRecognizer];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
